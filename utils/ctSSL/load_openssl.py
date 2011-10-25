f5446535a3ff8b0983d212dcdc576de181eea812
Alban D <nabla.c0d3@gmail.com>
2011-10-25 22:33:01 +0000

    Update to support FreeBSD and hopefully other things.
    M    utils/ctSSL/load_openssl.py

--- utils/ctSSL/load_openssl.py
+++ utils/ctSSL/load_openssl.py
@@ -56,17 +56,15 @@ elif os.name == 'posix':
         libcrypto_0_9_8 = 'libcrypto.so.0.9.8'
         libssl_0_9_8 = 'libssl.so.0.9.8'
 
-    elif sys.platform.startswith('freebsd'):
+    elif sys.platform == 'freebsd8':
         libcrypto_1_0_0 = 'libcrypto.so.7'
         libssl_1_0_0 = 'libssl.so.7'
         libcrypto_0_9_8 = 'libcrypto.so.6'
         libssl_0_9_8 = 'libssl.so.6'
 
-    else: # ?
-        libcrypto_1_0_0 = 'libcrypto.so.1.0.0'
-        libssl_1_0_0 = 'libssl.so.1.0.0'
-        libcrypto_0_9_8 = 'libcrypto.so.0.9.8'
-        libssl_0_9_8 = 'libssl.so.0.9.8'
+    else: 
+        libcrypto_1_0_0 = 'libcrypto.so'
+        libssl_1_0_0 = 'libssl.so'
 
 else:
     raise ctSSLInitError('OS not supported!')

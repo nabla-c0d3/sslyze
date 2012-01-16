ceb57dfe6fcb80877378ba02d46c5324bd679a9d
Alban D <nabla.c0d3@gmail.com>
2012-01-16 01:02:34 +0000

    New strategy to load OpenSSL libraries on Linux/Unix: use ctypes.util.find_library() instead of hardcoding lib names for each platform.
    Fixes issue 11

--- utils/ctSSL/load_openssl.py
+++ utils/ctSSL/load_openssl.py
@@ -13,6 +13,7 @@
 
 import ctypes
 from ctypes import c_ulong, c_char_p
+from ctypes.util import find_library
 import os
 import sys
 
@@ -25,69 +26,62 @@ class ctSSLInitError(Exception):
     pass
 
 
+
+def _load_openSSL_windows():
+    try: # Hopefully the DLLs are in the current folder
+        libcrypto = ctypes.CDLL('.\libeay32.dll', use_errno=True,use_last_error=True)
+        libssl = ctypes.CDLL('.\ssleay32.dll', use_errno=True, use_last_error=True)
+    except OSError:
+        try: # Else let Windows find the DLLs.
+            libcrypto = \
+                ctypes.CDLL('libeay32', use_errno=True, use_last_error=True)
+            libssl = ctypes.CDLL('ssleay32', use_errno=True, use_last_error=True)
+        except OSError:
+            raise ctSSLInitError('Could not load OpenSSL libraries.')
+        
+    return (libcrypto, libssl)
+
+
+def _load_openSSL_linux_default():
+    # On Linux we use find_library() to find the OpenSSL libraries
+    libcrypto_path = find_library('crypto')
+    libssl_path = find_library('ssl')
+    try:
+        libcrypto = ctypes.CDLL(libcrypto_path, use_errno=True,use_last_error=True)
+        libssl = ctypes.CDLL(libssl_path, use_errno=True, use_last_error=True)
+    except OSError:
+        raise ctSSLInitError('Could not load OpenSSL libraries.')
+    
+    return (libcrypto, libssl)   
+
+
 # Find the OpenSSL DLLs, depending on the platform
 if os.name == 'nt':
-# On Windows, the proper DLLs are expected to be in the current folder.
-    libcrypto_1_0_0 = '.\libeay32.dll' #Hopefully they are in the current folder
-    libssl_1_0_0 = '.\ssleay32.dll'
-    libcrypto_0_9_8 = 'libeay32' # Else, let the OS find them
-    libssl_0_9_8 = 'ssleay32'
+    (libcrypto, libssl) = _load_openSSL_windows()
 
 elif os.name == 'posix':
-    # Warning: on Linux, not specifying the version of the lib inside its name
-    # will make it go crazy
     if sys.platform == 'darwin': # MAC OS X
-        libcrypto_1_0_0 = 'libcrypto.1.0.0.dylib'
-        libssl_1_0_0 = 'libssl.1.0.0.dylib'
-        libcrypto_0_9_8 = 'libcrypto.0.9.8.dylib'
-        libssl_0_9_8 = 'libssl.0.9.8.dylib'
+        (libcrypto, libssl) = _load_openSSL_linux_default()
 
     elif sys.platform == 'cygwin':
-        # Hopefully they are in the current folder
-        libcrypto_1_0_0 = 'libeay32.dll'
-        libssl_1_0_0 = 'ssleay32.dll'
-        # Else, let the OS find them
-        libcrypto_0_9_8 = 'libeay32'
-        libssl_0_9_8 = 'ssleay32'
+        (libcrypto, libssl) = _load_openSSL_windows()
 
     elif sys.platform == 'linux2': # Any Linux
-        libcrypto_1_0_0 = 'libcrypto.so.1.0.0'
-        libssl_1_0_0 = 'libssl.so.1.0.0'
-        libcrypto_0_9_8 = 'libcrypto.so.0.9.8'
-        libssl_0_9_8 = 'libssl.so.0.9.8'
+        (libcrypto, libssl) = _load_openSSL_linux_default()
 
     elif sys.platform.startswith('freebsd'):
-        libcrypto_1_0_0 = 'libcrypto.so.7'
-        libssl_1_0_0 = 'libssl.so.7'
-        libcrypto_0_9_8 = 'libcrypto.so.6'
-        libssl_0_9_8 = 'libssl.so.6'
+        (libcrypto, libssl) = _load_openSSL_linux_default()
 
     else: # ?
-        libcrypto_1_0_0 = 'libcrypto.so.1.0.0'
-        libssl_1_0_0 = 'libssl.so.1.0.0'
-        libcrypto_0_9_8 = 'libcrypto.so.0.9.8'
-        libssl_0_9_8 = 'libssl.so.0.9.8'
+        (libcrypto, libssl) = _load_openSSL_linux_default()
 
 else:
     raise ctSSLInitError('OS not supported!')
 
-
-# Load the OpenSSL DLLs
-try: # We support OpenSSL 1.0.0
-    libcrypto = ctypes.CDLL(libcrypto_1_0_0, use_errno=True,use_last_error=True)
-    libssl = ctypes.CDLL(libssl_1_0_0, use_errno=True, use_last_error=True)
-except OSError:
-    try: # And OpenSSL 0.9.8
-        libcrypto = \
-            ctypes.CDLL(libcrypto_0_9_8, use_errno=True, use_last_error=True)
-        libssl = ctypes.CDLL(libssl_0_9_8, use_errno=True, use_last_error=True)
-    except OSError:
-        raise ctSSLInitError('Could not load OpenSSL 1.0.0 or 0.9.8.')
 
 # Store OpenSSL's version
 libcrypto.SSLeay.restype = c_ulong
 libcrypto.SSLeay_version.argtypes = [c_ulong]
 libcrypto.SSLeay_version.restype = c_char_p
 OpenSSL_version =  libcrypto.SSLeay()
-
 

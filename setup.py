926beaa12c0e17e949c362487014a6ec7521d654
Alban D. <nabla.c0d3@gmail.com>
2012-11-12 23:36:27 -0800

    Add distutils - setup.py
    Fixes issue #13.

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from sslyze import SSLYZE_VERSION, PROJECT_URL
+from distutils.core import setup
+
+setup(name='SSLyze',
+    version=SSLYZE_VERSION,
+    description='Fast and full-featured SSL scanner',
+    long_description=open('README.md').read(),
+    author_email='sslyze@isecpartners.com',
+    url=PROJECT_URL,
+    scripts=['sslyze.py'],
+    packages=['plugins', 'utils', 'utils.ctSSL'],
+    package_data={'plugins': ['data/mozilla_cacert.pem','data/mozilla_ev_oids.py']},
+    license=open('LICENSE.txt').read(),
+    )

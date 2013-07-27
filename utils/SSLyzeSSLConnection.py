5cd696bf6f60755d05d066ed3f2b41dd4a2e4954
Alban D <nabla.c0d3@gmail.com>
2013-07-27 15:28:44 -0700

    Don't silently catch unknown socket errors

--- utils/SSLyzeSSLConnection.py
+++ utils/SSLyzeSSLConnection.py
@@ -180,10 +180,12 @@ class SSLConnection(SslClient):
             for error_msg in self.HANDSHAKE_REJECTED_SOCKET_ERRORS.keys():
                 if error_msg in str(e.args):
                     raise SSLHandshakeRejected('TCP - ' + self.HANDSHAKE_REJECTED_SOCKET_ERRORS[error_msg])
+            raise # Unknown socket error
         
         except IOError as e:
             if 'Nassl SSL handshake failed' in str(e.args):
                 raise SSLHandshakeRejected('TLS - Unexpected EOF')
+            raise
         
         except _nassl.OpenSSLError as e:
             clientCertCaList = self.get_client_CA_list()

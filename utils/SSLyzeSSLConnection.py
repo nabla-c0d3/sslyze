d3fbbdbfe48f80af65ad9901e0c2d3eff9c3d2f9
Loic Jaquemet <loic.jaquemet+github@gmail.com>
2014-03-06 10:15:03 -0700

    Update SSLyzeSSLConnection.py

--- utils/SSLyzeSSLConnection.py
+++ utils/SSLyzeSSLConnection.py
@@ -98,7 +98,10 @@ def create_sslyze_connection(target, shared_settings, sslVersion=None, sslVerify
             sslConn = HTTPSTunnelConnection(target, sslVerifyLocations, timeout,
                                             shared_settings['nb_retries'],
                                             shared_settings['https_tunnel_host'],
-                                            shared_settings['https_tunnel_port'])
+                                            shared_settings['https_tunnel_port'],
+                                            shared_settings['https_tunnel_user'],
+                                            shared_settings['https_tunnel_password']
+                                            )
         else:
             sslConn = SSLTunnelConnection(target, sslVerifyLocations, timeout,
                                           shared_settings['nb_retries'],

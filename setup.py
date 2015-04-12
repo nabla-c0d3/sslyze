8db7964e23dfead7169d4fe3333b9ee8193fbeeb
Alban Diquet <nabla.c0d3@gmail.com>
2015-04-12 00:31:52 -0700

    Move OIDs back into the plugin to simplify module imports in py2exe builds

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ SSLYZE_SETUP = {
     'url' : PROJECT_URL,
     'scripts' : ['sslyze.py'],
     'packages' : ['plugins', 'utils', 'nassl'],
-    'package_data' : {'plugins' : ['data/trust_stores/*.pem','data/trust_stores/mozilla_ev_oids.py'],
+    'package_data' : {'plugins' : ['data/trust_stores/*.pem'],
                      'nassl' : [NASSL_BINARY]},
     'license' : open('LICENSE.txt').read()
 }

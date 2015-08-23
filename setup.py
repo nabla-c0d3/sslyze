31dee82de8f229d7baeb09605d61908bfa1a7188
em <mailto@zensecurity.su>
2015-08-23 23:57:43 +0300

    setup.py changed.

--- setup.py
+++ setup.py
@@ -11,14 +11,14 @@ from urllib import urlretrieve
 
 
 def setup():
-    if 'egg_info' in argv:
-        temp_dir = getcwd()
-        zlib_arch = 'zlib-1.2.8.tar.gz'
-        openssl_arch = 'openssl-1.0.2a.tar.gz'
-        nassl_dir = '{}/nassl.downloaded'.format(temp_dir)
-        nassl_build_dir = ''
+    try:
+        if 'egg_info' in argv:
+            temp_dir = getcwd()
+            zlib_arch = 'zlib-1.2.8.tar.gz'
+            openssl_arch = 'openssl-1.0.2a.tar.gz'
+            nassl_dir = '{}/nassl.downloaded'.format(temp_dir)
+            nassl_build_dir = ''
 
-        try:
             Popen(['git', 'clone', 'https://github.com/ZenSecurity/nassl.git', nassl_dir]).wait()
             urlretrieve('http://zlib.net/{}'.format(zlib_arch), '{}/{}'.format(nassl_dir, zlib_arch))
             tarfile_open('{}/{}'.format(nassl_dir, zlib_arch)).extractall(nassl_dir)
@@ -31,26 +31,26 @@ def setup():
                     nassl_build_dir = path_join(root, 'nassl')
 
             move(nassl_build_dir, "{}/nassl".format(temp_dir))
-        except Exception as exception:
-            print('{} - {}'.format(exception.__class__.__name__, exception))
 
-    NASSL_BINARY = '_nassl.so'
-    if platform == 'win32':
-            NASSL_BINARY = '_nassl.pyd'
+        NASSL_BINARY = '_nassl.so'
+        if platform == 'win32':
+                NASSL_BINARY = '_nassl.pyd'
 
-    from sslyze import PROJECT_VERSION, PROJECT_URL, PROJECT_EMAIL, PROJECT_DESC
+        from sslyze import PROJECT_VERSION, PROJECT_URL, PROJECT_EMAIL, PROJECT_DESC
 
-    distutils_setup(
-        name='SSLyze',
-        version=PROJECT_VERSION,
-        description=PROJECT_DESC,
-        long_description=open('README.md').read() + '\n' + open('AUTHORS.txt').read(),
-        author_email=PROJECT_EMAIL,
-        url=PROJECT_URL,
-        scripts=['sslyze.py'],
-        packages=['plugins', 'utils', 'nassl'],
-        package_data=dict(plugins=['data/trust_stores/*.pem'], nassl=[NASSL_BINARY]),
-        license=open('LICENSE.txt').read()
-    )
+        distutils_setup(
+            name='SSLyze',
+            version=PROJECT_VERSION,
+            description=PROJECT_DESC,
+            long_description=open('README.md').read() + '\n' + open('AUTHORS.txt').read(),
+            author_email=PROJECT_EMAIL,
+            url=PROJECT_URL,
+            scripts=['sslyze.py'],
+            packages=['plugins', 'utils', 'nassl'],
+            package_data=dict(plugins=['data/trust_stores/*.pem'], nassl=[NASSL_BINARY]),
+            license=open('LICENSE.txt').read()
+        )
+    except Exception as exception:
+        print('{} - {}'.format(exception.__class__.__name__, exception))
 
 setup()

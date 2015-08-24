3ecc02ccc4583f89cc33add4c790d821748f7c3a
em <mailto@zensecurity.su>
2015-08-24 12:04:57 +0300

    setup.py changed.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from distutils.core import setup as distutils_setup
 from os import walk, getcwd
-from os.path import join as path_join
+from os.path import isfile, join as path_join
 from shutil import move
 from subprocess import Popen, PIPE
 from sys import argv, platform
@@ -12,8 +12,9 @@ from urllib import urlretrieve
 
 def setup():
     try:
-        if 'egg_info' in argv:
-            temp_dir = getcwd()
+        temp_dir = getcwd()
+
+        if not isfile('{}/install.lock'.format(temp_dir)):
             zlib_arch = 'zlib-1.2.8.tar.gz'
             openssl_arch = 'openssl-1.0.2a.tar.gz'
             nassl_dir = '{}/nassl.downloaded'.format(temp_dir)
@@ -31,6 +32,8 @@ def setup():
                     nassl_build_dir = path_join(root, 'nassl')
 
             move(nassl_build_dir, "{}/nassl".format(temp_dir))
+
+            file('{}/install.lock'.format(temp_dir), 'w').close()
 
         NASSL_BINARY = '_nassl.so'
         if platform == 'win32':

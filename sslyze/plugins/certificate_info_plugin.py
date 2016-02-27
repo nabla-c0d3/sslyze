6b6b984b2d3982794b7ab8de0c549b876eb49224
Alban Diquet <nabla.c0d3@gmail.com>
2016-02-27 12:44:52 -0800

    Remove Google CA store because of #135

--- sslyze/plugins/certificate_info_plugin.py
+++ sslyze/plugins/certificate_info_plugin.py
@@ -60,7 +60,6 @@ DEFAULT_TRUST_STORE_LIST = [
     TrustStore(join(TRUST_STORES_PATH, 'microsoft.pem'), 'Microsoft', '02/2016'),
     TrustStore(join(TRUST_STORES_PATH, 'apple.pem'), 'Apple', 'OS X 10.11.3'),
     TrustStore(join(TRUST_STORES_PATH, 'java.pem'), 'Java 6', 'Update 65'),
-    TrustStore(join(TRUST_STORES_PATH, 'google.pem'), 'Google', '02/2016'),
 ]
 
 

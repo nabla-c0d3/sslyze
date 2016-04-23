6ebf51f803170681efbddf2704651b2cd0f2fc5e
Alban Diquet <nabla.c0d3@gmail.com>
2016-04-23 15:29:34 -0700

    Revert pull request #149
    This reverts commit 559346ffd7c032e3db71589fc3d6098fb4a206f6, reversing
    changes made to 83ec8d9f63c4b1a41930e90c9953c342757bb5fb.
    
    # Conflicts:
    #	sslyze/server_connectivity.py

--- sslyze/server_connectivity.py
+++ sslyze/server_connectivity.py
@@ -115,14 +115,9 @@ class ServerConnectivityInfo(object):
         self.ip_address = ip_address
         if not self.ip_address:
             try:
-                addr_infos = socket.getaddrinfo(self.hostname, self.port, socket.AF_UNSPEC, socket.IPPROTO_IP)
-                family, socktype, proto, canonname, sockaddr = addr_infos[0]
-            except (socket.gaierror, IndexError):
+                self.ip_address = socket.gethostbyname(self.hostname)
+            except socket.gaierror:
                 raise ServerConnectivityError(self.CONNECTIVITY_ERROR_NAME_NOT_RESOLVED.format(hostname=self.hostname))
-            else:
-                # Works for both IPv4 and IPv6
-                self.ip_address = sockaddr[0]
-                self.port = sockaddr[1]
 
         # Use the hostname as the default SNI
         self.tls_server_name_indication = tls_server_name_indication if tls_server_name_indication else self.hostname

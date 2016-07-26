8c4e4148adcd8923b4387dd9e699a38a282b37b8
Alban Diquet <ad@sourcetheorem.com>
2016-07-26 13:02:00 +0200

    Convert existing manual tests to automated tests

---
+++ tests/test_protocols.py
@@ -0,0 +1,85 @@
+import unittest
+
+from sslyze.plugins.certificate_info_plugin import CertificateInfoPlugin
+from sslyze.server_connectivity import ServerConnectivityInfo, ServerConnectivityError, \
+    ClientAuthenticationServerConfigurationEnum
+from sslyze.ssl_settings import TlsWrappedProtocolEnum
+
+
+class ProtocolsTestCase(unittest.TestCase):
+
+
+    def test_smtp_custom_port(self):
+        server_info = ServerConnectivityInfo(hostname='smtp.gmail.com', port=587,
+                                             tls_wrapped_protocol=TlsWrappedProtocolEnum.STARTTLS_SMTP)
+        server_info.test_connectivity_to_server()
+
+        plugin = CertificateInfoPlugin()
+        plugin_result = plugin.process_task(server_info, 'certinfo_basic')
+
+        self.assertEquals(len(plugin_result.certificate_chain), 3)
+
+        self.assertTrue(plugin_result.as_text())
+        self.assertTrue(plugin_result.as_xml())
+
+
+    def test_ipv6(self):
+        try:
+            server_info = ServerConnectivityInfo(hostname='www.ietf.org', ip_address='2001:1890:126c::1:1e')
+            server_info.test_connectivity_to_server()
+        except ServerConnectivityError as e:
+            print e
+            raise
+
+        plugin = CertificateInfoPlugin()
+        plugin_result = plugin.process_task(server_info, 'certinfo_basic')
+
+        self.assertEquals(len(plugin_result.certificate_chain), 2)
+
+        self.assertTrue(plugin_result.as_text())
+        self.assertTrue(plugin_result.as_xml())
+
+
+    def test_xmpp_to(self):
+        server_info = ServerConnectivityInfo(hostname='talk.google.com',
+                                             tls_wrapped_protocol=TlsWrappedProtocolEnum.STARTTLS_XMPP,
+                                             xmpp_to_hostname='gmail.com')
+        server_info.test_connectivity_to_server()
+
+        plugin = CertificateInfoPlugin()
+        plugin_result = plugin.process_task(server_info, 'certinfo_basic')
+
+        self.assertEquals(len(plugin_result.certificate_chain), 3)
+
+        self.assertTrue(plugin_result.as_text())
+        self.assertTrue(plugin_result.as_xml())
+
+
+
+    def test_starttls(self):
+        for hostname, protocol in [('imap.comcast.net', TlsWrappedProtocolEnum.STARTTLS_IMAP),
+                                   ('pop.comcast.net', TlsWrappedProtocolEnum.STARTTLS_POP3),
+                                   ('ldap.virginia.edu', TlsWrappedProtocolEnum.STARTTLS_LDAP),
+                                   ('jabber.org', TlsWrappedProtocolEnum.STARTTLS_XMPP_SERVER)]:
+
+            server_info = ServerConnectivityInfo(hostname=hostname, tls_wrapped_protocol=protocol)
+            server_info.test_connectivity_to_server()
+
+            plugin = CertificateInfoPlugin()
+            plugin_result = plugin.process_task(server_info, 'certinfo_basic')
+
+            self.assertTrue(plugin_result.as_text())
+            self.assertTrue(plugin_result.as_xml())
+
+
+    def test_optional_client_authentication(self):
+        for hostname in ['auth.startssl.com', 'xnet-eu.intellij.net']:
+            server_info = ServerConnectivityInfo(hostname=hostname)
+            server_info.test_connectivity_to_server()
+            self.assertEquals(server_info.client_auth_requirement, ClientAuthenticationServerConfigurationEnum.OPTIONAL)
+
+            plugin = CertificateInfoPlugin()
+            plugin_result = plugin.process_task(server_info, 'certinfo_basic')
+
+            self.assertTrue(plugin_result.as_text())
+            self.assertTrue(plugin_result.as_xml())

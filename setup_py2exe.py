b6d48a8440bc50a3c345732d702bfee1b6da391a
Alban Diquet <nabla.c0d3@gmail.com>
2017-02-12 21:08:21 -0800

    Fix py2exe builds

--- setup_py2exe.py
+++ setup_py2exe.py
@@ -7,6 +7,11 @@ import os
 import py2exe
 from setup import SSLYZE_SETUP
 
+# Add lib to the path so py2exe can find the dependencies
+import sys
+sys.path.insert(1, os.path.join(os.path.dirname(__file__), 'lib'))
+
+
 data_files = [
     ("Microsoft.VC90.CRT", glob(r'C:\Program Files\Microsoft Visual Studio 9.0\VC\redist\x86\Microsoft.VC90.CRT\*.*'))]
 
@@ -18,23 +23,21 @@ for file in os.listdir(trust_stores_pem_path):
     if os.path.isfile(file):  # skip directories
         plugin_data_files.append(file)
 
-data_files.append((os.path.join('utils', 'trust_store', 'pem_files'), plugin_data_files))
+data_files.append((os.path.join('pem_files'), plugin_data_files))
 
 sslyze_setup_py2exe = SSLYZE_SETUP.copy()
-# Add nassl to the list of packages
-sslyze_setup_py2exe['packages'].append('nassl')
+
 sslyze_setup_py2exe.update(
         {
-            # Force the packaging of the C extension
-            'package_data': {'nassl': ['_nassl.pyd']},
-            'console': ['sslyze\\__main__.py'],
+            'console': [os.path.join('sslyze', '__main__.py')],
+            # Add dependencies
+            'include': ['nassl', 'typing', 'enum'],
             'data_files': data_files,
             'zipfile': None,
             'options': {'py2exe': {
                 # 'skip_archive': True,
                 'bundle_files': 1,
             }}
-
         }
 )
 

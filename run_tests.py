ebc4211d882cf88e5ee1f94b3ea45d9a0d1afe27
Alban Diquet <nabla.c0d3@gmail.com>
2017-04-15 16:15:04 -0700

    Travis CI

--- run_tests.py
+++ run_tests.py
@@ -8,7 +8,7 @@ def main(test_path):
     # Add nassl to the path
     sys.path.insert(1, os.path.join(os.path.dirname(__file__), 'lib'))
 
-    suite = unittest.loader.TestLoader().discover(test_path)
+    suite = unittest.loader.TestLoader().discover(test_path, pattern='*cipher*')
     result = unittest.TextTestRunner(verbosity=2).run(suite)
     exit_code = 0 if result.wasSuccessful() else 1
     sys.exit(exit_code)

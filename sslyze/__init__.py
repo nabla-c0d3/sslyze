1b97e4f8cdbcaa8873aa2f5f6172ca08b8cbd300
Alban Diquet <nabla.c0d3@gmail.com>
2017-04-15 20:05:38 -0700

    Bump version number

--- sslyze/__init__.py
+++ sslyze/__init__.py
@@ -1,7 +1,7 @@
 
-__author__ = u'Alban Diquet'
-__license__ = u'GPLv2'
-__version__ = u'1.0.0'
-__email__ = u'nabla.c0d3@gmail.com'
-PROJECT_URL = u'https://github.com/nabla-c0d3/sslyze'
-PROJECT_DESC = u'Fast and full-featured SSL scanner.'
+__author__ = 'Alban Diquet'
+__license__ = 'GPLv2'
+__version__ = '1.1.0'
+__email__ = 'nabla.c0d3@gmail.com'
+PROJECT_URL = 'https://github.com/nabla-c0d3/sslyze'
+PROJECT_DESC = 'Fast and full-featured SSL scanner.'

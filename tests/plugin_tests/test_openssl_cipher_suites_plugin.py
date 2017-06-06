61dd65a05df4a6e397a3f7d2b868f8ece0fe6bf3
Szilárd Pfeiffer <coroner@pfeifferszilard.hu>
2017-06-06 14:30:04 +0200

    Refactorize cipher suite plugin tests

--- tests/plugin_tests/test_openssl_cipher_suites_plugin.py
+++ tests/plugin_tests/test_openssl_cipher_suites_plugin.py
@@ -18,6 +18,22 @@ from tests.plugin_tests.openssl_server import VulnerableOpenSslServer
 
 class OpenSslCipherSuitesPluginTestCase(unittest.TestCase):
 
+    def _get_plugin_result(self, hostname, command=Tlsv12ScanCommand()):
+        server_info = ServerConnectivityInfo(hostname=hostname)
+        server_info.test_connectivity_to_server()
+
+        plugin = OpenSslCipherSuitesPlugin()
+        plugin_result = plugin.process_task(server_info, command)
+
+        return plugin_result
+
+    def _test_plugin_outputs(self, plugin_result):
+        self.assertTrue(plugin_result.as_text())
+        self.assertTrue(plugin_result.as_xml())
+
+        # Ensure the results are pickable so the ConcurrentScanner can receive them via a Queue
+        self.assertTrue(pickle.dumps(plugin_result))
+
     @unittest.skipIf(NOT_ON_LINUX_64BIT,
                      'test suite only has the vulnerable OpenSSL version compiled for Linux 64 bits')
     def test_sslv2_enabled(self):
@@ -43,29 +59,17 @@ class OpenSslCipherSuitesPluginTestCase(unittest.TestCase):
         self.assertFalse(plugin_result.rejected_cipher_list)
         self.assertFalse(plugin_result.errored_cipher_list)
 
-        self.assertTrue(plugin_result.as_text())
-        self.assertTrue(plugin_result.as_xml())
-
-        # Ensure the results are pickable so the ConcurrentScanner can receive them via a Queue
-        self.assertTrue(pickle.dumps(plugin_result))
+        self._test_plugin_outputs(plugin_result)
 
     def test_sslv2_disabled(self):
-        server_info = ServerConnectivityInfo(hostname='www.google.com')
-        server_info.test_connectivity_to_server()
-
-        plugin = OpenSslCipherSuitesPlugin()
-        plugin_result = plugin.process_task(server_info, Sslv20ScanCommand())
+        plugin_result = self._get_plugin_result('www.google.com', Sslv20ScanCommand())
 
         self.assertIsNone(plugin_result.preferred_cipher)
         self.assertFalse(plugin_result.accepted_cipher_list)
         self.assertTrue(plugin_result.rejected_cipher_list)
         self.assertFalse(plugin_result.errored_cipher_list)
 
-        self.assertTrue(plugin_result.as_text())
-        self.assertTrue(plugin_result.as_xml())
-
-        # Ensure the results are pickable so the ConcurrentScanner can receive them via a Queue
-        self.assertTrue(pickle.dumps(plugin_result))
+        self._test_plugin_outputs(plugin_result)
 
     @unittest.skipIf(NOT_ON_LINUX_64BIT,
                      'test suite only has the vulnerable OpenSSL version compiled for Linux 64 bits')
@@ -106,18 +110,10 @@ class OpenSslCipherSuitesPluginTestCase(unittest.TestCase):
         self.assertTrue(plugin_result.rejected_cipher_list)
         self.assertFalse(plugin_result.errored_cipher_list)
 
-        self.assertTrue(plugin_result.as_text())
-        self.assertTrue(plugin_result.as_xml())
-
-        # Ensure the results are pickable so the ConcurrentScanner can receive them via a Queue
-        self.assertTrue(pickle.dumps(plugin_result))
+        self._test_plugin_outputs(plugin_result)
 
     def test_sslv3_disabled(self):
-        server_info = ServerConnectivityInfo(hostname='www.google.com')
-        server_info.test_connectivity_to_server()
-
-        plugin = OpenSslCipherSuitesPlugin()
-        plugin_result = plugin.process_task(server_info, Sslv30ScanCommand())
+        plugin_result = self._get_plugin_result('www.google.com', Sslv30ScanCommand())
 
         self.assertIsNone(plugin_result.preferred_cipher)
         self.assertFalse(plugin_result.accepted_cipher_list)
@@ -131,11 +127,7 @@ class OpenSslCipherSuitesPluginTestCase(unittest.TestCase):
         self.assertTrue(pickle.dumps(plugin_result))
 
     def test_tlsv1_0_enabled(self):
-        server_info = ServerConnectivityInfo(hostname='www.google.com')
-        server_info.test_connectivity_to_server()
-
-        plugin = OpenSslCipherSuitesPlugin()
-        plugin_result = plugin.process_task(server_info, Tlsv10ScanCommand())
+        plugin_result = self._get_plugin_result('www.google.com', Tlsv10ScanCommand())
 
         self.assertTrue(plugin_result.preferred_cipher)
         accepted_cipher_name_list = [cipher.name for cipher in plugin_result.accepted_cipher_list]
@@ -148,11 +140,7 @@ class OpenSslCipherSuitesPluginTestCase(unittest.TestCase):
         self.assertTrue(plugin_result.rejected_cipher_list)
         self.assertFalse(plugin_result.errored_cipher_list)
 
-        self.assertTrue(plugin_result.as_text())
-        self.assertTrue(plugin_result.as_xml())
-
-        # Ensure the results are pickable so the ConcurrentScanner can receive them via a Queue
-        self.assertTrue(pickle.dumps(plugin_result))
+        self._test_plugin_outputs(plugin_result)
 
     def test_tlsv1_0_disabled(self):
         # TBD
@@ -160,11 +148,7 @@ class OpenSslCipherSuitesPluginTestCase(unittest.TestCase):
 
 
     def test_tlsv1_1_enabled(self):
-        server_info = ServerConnectivityInfo(hostname='www.google.com')
-        server_info.test_connectivity_to_server()
-
-        plugin = OpenSslCipherSuitesPlugin()
-        plugin_result = plugin.process_task(server_info, Tlsv11ScanCommand())
+        plugin_result = self._get_plugin_result('www.google.com', Tlsv11ScanCommand())
 
         self.assertTrue(plugin_result.preferred_cipher)
         self.assertTrue(plugin_result.accepted_cipher_list)
@@ -177,19 +161,10 @@ class OpenSslCipherSuitesPluginTestCase(unittest.TestCase):
         self.assertTrue(plugin_result.rejected_cipher_list)
         self.assertFalse(plugin_result.errored_cipher_list)
 
-        self.assertTrue(plugin_result.as_text())
-        self.assertTrue(plugin_result.as_xml())
-
-        # Ensure the results are pickable so the ConcurrentScanner can receive them via a Queue
-        self.assertTrue(pickle.dumps(plugin_result))
+        self._test_plugin_outputs(plugin_result)
 
     def test_tlsv1_2_enabled(self):
-        server_info = ServerConnectivityInfo(hostname='www.google.com')
-        server_info.test_connectivity_to_server()
-
-        plugin = OpenSslCipherSuitesPlugin()
-        # Also do full HTTP connections
-        plugin_result = plugin.process_task(server_info, Tlsv12ScanCommand(http_get=True))
+        plugin_result = self._get_plugin_result('www.google.com', Tlsv12ScanCommand(http_get=True))
 
         self.assertTrue(plugin_result.preferred_cipher)
         self.assertTrue(plugin_result.accepted_cipher_list)
@@ -206,18 +181,10 @@ class OpenSslCipherSuitesPluginTestCase(unittest.TestCase):
         self.assertTrue(plugin_result.rejected_cipher_list)
         self.assertFalse(plugin_result.errored_cipher_list)
 
-        self.assertTrue(plugin_result.as_text())
-        self.assertTrue(plugin_result.as_xml())
-
-        # Ensure the results are pickable so the ConcurrentScanner can receive them via a Queue
-        self.assertTrue(pickle.dumps(plugin_result))
+        self._test_plugin_outputs(plugin_result)
 
     def test_null_cipher_suites(self):
-        server_info = ServerConnectivityInfo(hostname='null.badssl.com')
-        server_info.test_connectivity_to_server()
-
-        plugin = OpenSslCipherSuitesPlugin()
-        plugin_result = plugin.process_task(server_info, Tlsv12ScanCommand())
+        plugin_result = self._get_plugin_result('null.badssl.com')
 
         accepted_cipher_name_list = [cipher.name for cipher in plugin_result.accepted_cipher_list]
         self.assertEqual({'TLS_ECDH_anon_WITH_AES_256_CBC_SHA', 'TLS_DH_anon_WITH_AES_256_CBC_SHA256',
@@ -229,53 +196,29 @@ class OpenSslCipherSuitesPluginTestCase(unittest.TestCase):
                           'TLS_ECDH_anon_WITH_NULL_SHA', 'TLS_RSA_WITH_NULL_SHA256', 'TLS_RSA_WITH_NULL_SHA'},
                           set(accepted_cipher_name_list))
 
-        self.assertTrue(plugin_result.as_text())
-        self.assertTrue(plugin_result.as_xml())
-
-        # Ensure the results are pickable so the ConcurrentScanner can receive them via a Queue
-        self.assertTrue(pickle.dumps(plugin_result))
+        self._test_plugin_outputs(plugin_result)
 
     def test_rc4_cipher_suites(self):
-        server_info = ServerConnectivityInfo(hostname='rc4.badssl.com')
-        server_info.test_connectivity_to_server()
-
-        plugin = OpenSslCipherSuitesPlugin()
-        plugin_result = plugin.process_task(server_info, Tlsv12ScanCommand())
+        plugin_result = self._get_plugin_result('rc4.badssl.com')
 
         accepted_cipher_name_list = [cipher.name for cipher in plugin_result.accepted_cipher_list]
         self.assertEqual({'TLS_ECDHE_RSA_WITH_RC4_128_SHA', 'TLS_RSA_WITH_RC4_128_SHA'},
                          set(accepted_cipher_name_list))
 
-        self.assertTrue(plugin_result.as_text())
-        self.assertTrue(plugin_result.as_xml())
-
-        # Ensure the results are pickable so the ConcurrentScanner can receive them via a Queue
-        self.assertTrue(pickle.dumps(plugin_result))
+        self._test_plugin_outputs(plugin_result)
 
     def test_rc4_md5_cipher_suites(self):
-        server_info = ServerConnectivityInfo(hostname='rc4-md5.badssl.com')
-        server_info.test_connectivity_to_server()
-
-        plugin = OpenSslCipherSuitesPlugin()
-        plugin_result = plugin.process_task(server_info, Tlsv12ScanCommand())
+        plugin_result = self._get_plugin_result('rc4-md5.badssl.com')
 
         accepted_cipher_name_list = [cipher.name for cipher in plugin_result.accepted_cipher_list]
         self.assertEqual({'TLS_RSA_WITH_RC4_128_MD5'},
                          set(accepted_cipher_name_list))
 
-        self.assertTrue(plugin_result.as_text())
-        self.assertTrue(plugin_result.as_xml())
-
-        # Ensure the results are pickable so the ConcurrentScanner can receive them via a Queue
-        self.assertTrue(pickle.dumps(plugin_result))
+        self._test_plugin_outputs(plugin_result)
 
     def test_follows_client_cipher_suite_preference(self):
         # Google.com does not follow client cipher suite preference
-        server_info = ServerConnectivityInfo(hostname='www.google.com')
-        server_info.test_connectivity_to_server()
-
-        plugin = OpenSslCipherSuitesPlugin()
-        plugin_result = plugin.process_task(server_info, Tlsv12ScanCommand())
+        plugin_result = self._get_plugin_result('www.google.com')
 
         self.assertTrue(plugin_result.preferred_cipher)
         self.assertTrue(plugin_result.accepted_cipher_list)
@@ -287,11 +230,7 @@ class OpenSslCipherSuitesPluginTestCase(unittest.TestCase):
         plugin = OpenSslCipherSuitesPlugin()
         plugin_result = plugin.process_task(server_info, Tlsv12ScanCommand())
 
-        self.assertIsNone(plugin_result.preferred_cipher)
-        self.assertTrue(plugin_result.accepted_cipher_list)
-
-        # Ensure the results are pickable so the ConcurrentScanner can receive them via a Queue
-        self.assertTrue(pickle.dumps(plugin_result))
+        self._test_plugin_outputs(plugin_result)
 
     def test_smtp_post_handshake_response(self):
         server_info = ServerConnectivityInfo(hostname='smtp.gmail.com', port=587,
@@ -301,8 +240,7 @@ class OpenSslCipherSuitesPluginTestCase(unittest.TestCase):
         plugin = OpenSslCipherSuitesPlugin()
         plugin_result = plugin.process_task(server_info, Tlsv12ScanCommand())
 
-        self.assertTrue(plugin_result.as_text())
-        self.assertTrue(plugin_result.as_xml())
+        self._test_plugin_outputs(plugin_result)
 
     def test_tls_1_3_cipher_suites(self):
         server_info = ServerConnectivityInfo(hostname='www.cloudflare.com')

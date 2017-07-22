6d8a993be3b0469fb26dd52a44b372445dffa7e9
Alban Diquet <nabla.c0d3@gmail.com>
2017-07-22 10:46:19 -0700

    Add separate test for EV certs and OCSP Stapling

--- tests/plugin_tests/test_certificate_info_plugin.py
+++ tests/plugin_tests/test_certificate_info_plugin.py
@@ -38,7 +38,7 @@ class CertificateInfoPluginTestCase(unittest.TestCase):
                 self.assertTrue(path_validation_result.is_certificate_trusted)
 
 
-    def test_valid_chain(self):
+    def test_valid_chain_with_ocsp_stapling(self):
         server_info = ServerConnectivityInfo(hostname='login.live.com')
         server_info.test_connectivity_to_server()
 
@@ -47,9 +47,36 @@ class CertificateInfoPluginTestCase(unittest.TestCase):
 
         self.assertTrue(plugin_result.ocsp_response)
         self.assertTrue(plugin_result.is_ocsp_response_trusted)
-        self.assertTrue(plugin_result.is_leaf_certificate_ev)
 
         self.assertEquals(len(plugin_result.certificate_chain), 2)
+        self.assertEquals(len(plugin_result.verified_certificate_chain), 3)
+        self.assertFalse(plugin_result.has_anchor_in_certificate_chain)
+
+        self.assertEquals(len(plugin_result.path_validation_result_list), 5)
+        for path_validation_result in plugin_result.path_validation_result_list:
+            self.assertTrue(path_validation_result.is_certificate_trusted)
+
+        self.assertEquals(len(plugin_result.path_validation_error_list), 0)
+        self.assertEquals(plugin_result.certificate_matches_hostname, True)
+        self.assertTrue(plugin_result.is_certificate_chain_order_valid)
+
+        self.assertTrue(plugin_result.as_text())
+        self.assertTrue(plugin_result.as_xml())
+
+        # Ensure the results are pickable so the ConcurrentScanner can receive them via a Queue
+        self.assertTrue(pickle.dumps(plugin_result))
+
+
+    def test_valid_chain_with_ev_cert(self):
+        server_info = ServerConnectivityInfo(hostname='www.comodo.com')
+        server_info.test_connectivity_to_server()
+
+        plugin = CertificateInfoPlugin()
+        plugin_result = plugin.process_task(server_info, CertificateInfoScanCommand())
+
+        self.assertTrue(plugin_result.is_leaf_certificate_ev)
+
+        self.assertEquals(len(plugin_result.certificate_chain), 3)
         self.assertEquals(len(plugin_result.verified_certificate_chain), 3)
         self.assertFalse(plugin_result.has_anchor_in_certificate_chain)
 

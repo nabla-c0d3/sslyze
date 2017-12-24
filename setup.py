e3b1696e77e29fd08575afe59a51ef867201aeed
Alban Diquet <nabla.c0d3@gmail.com>
2017-12-23 16:26:04 -0800

    Python 3.3 is no longer supported

--- setup.py
+++ setup.py
@@ -27,7 +27,6 @@ SSLYZE_SETUP = {
         'License :: OSI Approved :: GNU General Public License v2 (GPLv2)',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',

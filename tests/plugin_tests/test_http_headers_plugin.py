d46c2ef6bf1e2a8a022558adc2fea84d2e40174c
Alban Diquet <ad@sourcetheorem.com>
2018-01-14 17:55:00 +0100

    Disable HPKP test

--- tests/plugin_tests/test_http_headers_plugin.py
+++ tests/plugin_tests/test_http_headers_plugin.py
@@ -47,19 +47,5 @@ class HttpHeadersPluginTestCase(unittest.TestCase):
         self.assertTrue(pickle.dumps(plugin_result))
 
     def test_hpkp_enabled(self):
-        server_info = ServerConnectivityInfo(hostname='github.com')
-        server_info.test_connectivity_to_server()
-
-        plugin = HttpHeadersPlugin()
-        plugin_result = plugin.process_task(server_info, HttpHeadersScanCommand())
-
-        self.assertTrue(plugin_result.hpkp_header)
-        self.assertTrue(plugin_result.is_valid_pin_configured)
-        self.assertTrue(plugin_result.is_backup_pin_configured)
-        self.assertTrue(plugin_result.verified_certificate_chain)
-
-        self.assertTrue(plugin_result.as_text())
-        self.assertTrue(plugin_result.as_xml())
-
-        # Ensure the results are pickable so the ConcurrentScanner can receive them via a Queue
-        self.assertTrue(pickle.dumps(plugin_result))
+        # HPKP is being deprecated in Chrome - I couldn't find a website with the header set
+        pass

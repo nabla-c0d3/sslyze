2ff5cdd4843ed54ac6fe8b505db15e7c6cb62157
Alban Diquet <nabla.c0d3@gmail.com>
2018-03-04 15:52:25 -0800

    Add more tests for client auth (#289)

--- tests/plugin_tests/test_fallback_scsv_plugin.py
+++ tests/plugin_tests/test_fallback_scsv_plugin.py
@@ -12,7 +12,7 @@ from nassl.ssl_client import ClientCertificateRequested
 
 from sslyze.plugins.fallback_scsv_plugin import FallbackScsvPlugin, FallbackScsvScanCommand
 from sslyze.server_connectivity_tester import ServerConnectivityTester
-from sslyze.ssl_settings import ClientAuthenticationServerConfigurationEnum
+from sslyze.ssl_settings import ClientAuthenticationServerConfigurationEnum, ClientAuthenticationCredentials
 from tests.openssl_server import NotOnLinux64Error
 from tests.openssl_server import VulnerableOpenSslServer
 
@@ -77,3 +77,36 @@ class FallbackScsvPluginTestCase(unittest.TestCase):
         except NotOnLinux64Error:
             logging.warning('WARNING: Not on Linux - skipping test')
             return
+
+    def test_works_when_client_auth_succeeded(self):
+        # Given a server that requires client authentication
+        try:
+            with VulnerableOpenSslServer(
+                    client_auth_config=ClientAuthenticationServerConfigurationEnum.REQUIRED
+            ) as server:
+                # And the client provides a client certificate
+                client_creds = ClientAuthenticationCredentials(
+                    client_certificate_chain_path=server.get_client_certificate_path(),
+                    client_key_path=server.get_client_key_path(),
+                )
+
+                server_test = ServerConnectivityTester(
+                    hostname=server.hostname,
+                    ip_address=server.ip_address,
+                    port=server.port,
+                    client_auth_credentials=client_creds,
+                )
+                server_info = server_test.perform()
+
+                # The plugin works fine
+                plugin = FallbackScsvPlugin()
+                plugin_result = plugin.process_task(server_info, FallbackScsvScanCommand())
+
+        except NotOnLinux64Error:
+            logging.warning('WARNING: Not on Linux - skipping test')
+            return
+
+        self.assertFalse(plugin_result.supports_fallback_scsv)
+
+        self.assertTrue(plugin_result.as_text())
+        self.assertTrue(plugin_result.as_xml())

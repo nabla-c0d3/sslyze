b48d929a251f79f930c7bf9ced0b7236d6a13b4d
Alban Diquet <nabla.c0d3@gmail.com>
2018-05-19 18:07:21 -0700

    Update the setup file and merge cx_freeze config

--- sslyze/__init__.py
+++ sslyze/__init__.py
@@ -2,5 +2,3 @@
 __author__ = 'Alban Diquet'
 __version__ = '1.4.2'
 __email__ = 'nabla.c0d3@gmail.com'
-PROJECT_URL = 'https://github.com/nabla-c0d3/sslyze'
-PROJECT_DESC = 'Fast and full-featured SSL scanner.'

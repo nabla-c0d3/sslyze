778523140e90cd33dcff0c166fe3f761aa305fd1
Alban Diquet <nabla.c0d3@gmail.com>
2018-05-20 12:05:58 -0700

    Add task for releasing

--- tasks.py
+++ tasks.py
@@ -2,6 +2,9 @@ from pathlib import Path
 from sys import platform
 
 from invoke import task
+from sslyze import __version__
+
+root_path = Path(__file__).parent
 
 
 @task
@@ -10,16 +13,30 @@ def test(ctx):
     ctx.run('flake8 sslyze')
     ctx.run('mypy sslyze')
 
+
 @task
 def gen_doc(ctx):
-    docs_folder_path = Path(__file__).parent / 'docs'
+    docs_folder_path = root_path / 'docs'
     dst_path = docs_folder_path / 'documentation'
     ctx.run(f'python -m sphinx -v -b html {docs_folder_path} {dst_path}')
 
 
 @task
 def release(ctx):
-    pass
+    response = input(f'Release version "{__version__}" ? y/n')
+    if response.lower() != 'y':
+        print('Cancelled')
+        return
+
+    test(ctx)
+
+    # Add the git tag
+    ctx.run(f"git tag -a {__version__} -m '{__version__}'")
+    ctx.run('git push --tags')
+
+    # Upload to Pypi
+    sdist_path = root_path / 'dist' / f'sslyze-{__version__}.tar.gz'
+    ctx.run(f'twine upload {sdist_path}')
 
 @task
 def build_exe(ctx):

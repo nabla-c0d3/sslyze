b0ca659606b87533de6495b7a09f9ec4dfd8f28e
Alban Diquet <nabla.c0d3@gmail.com>
2019-01-31 13:17:32 -0800

    Update module in setup.py

--- setup.py
+++ setup.py
@@ -80,7 +80,7 @@ setup(
     # Dependencies
     install_requires=[
         'nassl>=2.1.0,<2.2.0',
-        'cryptography==2.3',
+        'cryptography==2.5',
         'tls-parser>=1.2.0,<1.3.0'
     ],
 

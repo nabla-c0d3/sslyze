ffc1a3915db6ecb364a0d4d6748d22c12ba5f64b
Alban Diquet <nabla.c0d3@gmail.com>
2019-06-05 23:01:21 -0700

    [#375] Simplify and fix JSON output

--- sslyze/cli/json_output.py
+++ sslyze/cli/json_output.py
@@ -1,4 +1,5 @@
 import json
+from pathlib import Path
 from typing import Dict, Any, TextIO, Type, Set, Union, List
 
 from cryptography.hazmat.backends.openssl import x509
@@ -12,6 +13,7 @@ from sslyze.cli.command_line_parser import ServerStringParsingError
 from sslyze.cli.output_generator import OutputGenerator
 from sslyze.plugins.plugin_base import Plugin
 from sslyze.plugins.utils.certificate_utils import CertificateUtils
+from sslyze.plugins.utils.trust_store.trust_store import TrustStore
 from sslyze.server_connectivity_info import ServerConnectivityInfo
 from sslyze.server_connectivity_tester import ServerConnectivityError
 
@@ -50,23 +52,17 @@ class JsonOutputGenerator(OutputGenerator):
 
     def server_scan_completed(self, server_scan_result: CompletedServerScan) -> None:
         server_scan_dict = {'server_info': server_scan_result.server_info.__dict__.copy()}
-        for key, value in server_scan_dict['server_info'].items():
-            server_scan_dict['server_info'][key] = _object_to_json_dict(value)
 
         dict_command_result: Dict[str, Dict[str, Any]] = {}
         for plugin_result in server_scan_result.plugin_result_list:
             dict_result = plugin_result.__dict__.copy()
-            # Remove the server_info node
-            dict_result.pop('server_info', None)
 
-            # Remove the scan_command node
+            # Remove nodes we don't want
+            dict_result.pop('server_info', None)
             scan_command = dict_result.pop('scan_command', None)
 
             if scan_command.get_cli_argument() in dict_command_result.keys():
                 raise ValueError('Received duplicate result for command {}'.format(scan_command))
-
-            for key, value in dict_result.items():
-                dict_result[key] = _object_to_json_dict(value)
 
             dict_command_result[scan_command.get_cli_argument()] = dict_result
 
@@ -76,70 +72,70 @@ class JsonOutputGenerator(OutputGenerator):
     def scans_completed(self, total_scan_time: float) -> None:
         self._json_dict['total_scan_time'] = str(total_scan_time)
         json_out = json.dumps(
-            self._json_dict, default=_object_to_json_dict, sort_keys=True, indent=4, ensure_ascii=True
+            self._json_dict, cls=_CustomJsonEncoder, sort_keys=True, indent=4, ensure_ascii=True
         )
         self._file_to.write(json_out)
 
 
-def _object_to_json_dict(obj: Any) -> Union[bool, int, float, str, Dict[str, Any]]:
-    """Convert an object to a dictionary suitable for the JSON output.
-    """
-    if isinstance(obj, Enum):
-        # Properly serialize Enums (such as OpenSslVersionEnum)
-        result = obj.name
+class _CustomJsonEncoder(json.JSONEncoder):
 
-    elif isinstance(obj, ObjectIdentifier):
-        # Use dotted string representation for OIDs
-        result = obj.dotted_string
+    def default(self, obj: Any) -> Union[bool, int, float, str, Dict[str, Any]]:
+        if isinstance(obj, Enum):
+            result = obj.name
 
-    elif isinstance(obj, x509._Certificate):
-        # Properly serialize certificates
-        certificate = obj
-        result = {  # type: ignore
-            # Add general info
-            'as_pem': obj.public_bytes(Encoding.PEM).decode('ascii'),
-            'hpkp_pin': CertificateUtils.get_hpkp_pin(obj),
+        elif isinstance(obj, ObjectIdentifier):
+            result = obj.dotted_string
 
-            # Add some of the fields of the cert
-            'subject': CertificateUtils.get_name_as_text(certificate.subject),
-            'issuer': CertificateUtils.get_name_as_text(certificate.issuer),
-            'serialNumber': str(certificate.serial_number),
-            'notBefore': certificate.not_valid_before.strftime("%Y-%m-%d %H:%M:%S"),
-            'notAfter': certificate.not_valid_after.strftime("%Y-%m-%d %H:%M:%S"),
-            'signatureAlgorithm': certificate.signature_hash_algorithm.name,
-            'publicKey': {
-                'algorithm': CertificateUtils.get_public_key_type(certificate)
-            },
-        }
+        elif isinstance(obj, x509._Certificate):
+            certificate = obj
+            result = {  # type: ignore
+                # Add general info
+                'as_pem': obj.public_bytes(Encoding.PEM).decode('ascii'),
+                'hpkp_pin': CertificateUtils.get_hpkp_pin(obj),
 
-        dns_alt_names = CertificateUtils.get_dns_subject_alternative_names(certificate)
-        if dns_alt_names:
-            result['subjectAlternativeName'] = {'DNS': dns_alt_names}  # type: ignore
+                # Add some of the fields of the cert
+                'subject': CertificateUtils.get_name_as_text(certificate.subject),
+                'issuer': CertificateUtils.get_name_as_text(certificate.issuer),
+                'serialNumber': str(certificate.serial_number),
+                'notBefore': certificate.not_valid_before.strftime("%Y-%m-%d %H:%M:%S"),
+                'notAfter': certificate.not_valid_after.strftime("%Y-%m-%d %H:%M:%S"),
+                'signatureAlgorithm': certificate.signature_hash_algorithm.name,
+                'publicKey': {
+                    'algorithm': CertificateUtils.get_public_key_type(certificate)
+                },
+            }
 
-        # Add some info about the public key
-        public_key = certificate.public_key()
-        if isinstance(public_key, EllipticCurvePublicKey):
-            result['publicKey']['size'] = str(public_key.curve.key_size)  # type: ignore
-            result['publicKey']['curve'] = public_key.curve.name  # type: ignore
+            dns_alt_names = CertificateUtils.get_dns_subject_alternative_names(certificate)
+            if dns_alt_names:
+                result['subjectAlternativeName'] = {'DNS': dns_alt_names}  # type: ignore
+
+            # Add some info about the public key
+            public_key = certificate.public_key()
+            if isinstance(public_key, EllipticCurvePublicKey):
+                result['publicKey']['size'] = str(public_key.curve.key_size)  # type: ignore
+                result['publicKey']['curve'] = public_key.curve.name  # type: ignore
+            else:
+                result['publicKey']['size'] = str(public_key.key_size)
+                result['publicKey']['exponent'] = str(public_key.public_numbers().e)
+
+        elif isinstance(obj, Path):
+            result = str(obj)
+
+        elif isinstance(obj, object):
+            # Some objects (like str) don't have a __dict__
+            if hasattr(obj, '__dict__'):
+                result = {}
+                for key, value in obj.__dict__.items():
+                    # Remove private attributes
+                    if key.startswith('_'):
+                        continue
+
+                    result[key] = self.default(value)
+            else:
+                # Simple object like a bool
+                result = obj
+
         else:
-            result['publicKey']['size'] = str(public_key.key_size)
-            result['publicKey']['exponent'] = str(public_key.public_numbers().e)
+            raise TypeError('Unknown type: {}'.format(repr(obj)))
 
-    elif isinstance(obj, object):
-        # Some objects (like str) don't have a __dict__
-        if hasattr(obj, '__dict__'):
-            result = {}
-            for key, value in obj.__dict__.items():
-                # Remove private attributes
-                if key.startswith('_'):
-                    continue
-
-                result[key] = _object_to_json_dict(value)
-        else:
-            # Simple object like a bool
-            result = obj
-
-    else:
-        raise TypeError('Unknown type: {}'.format(repr(obj)))
-
-    return result
+        return result

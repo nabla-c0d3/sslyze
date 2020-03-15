af0dd27707a358701c01a38bf64ab6104393718e
Alban Diquet <nabla.c0d3@gmail.com>
2020-03-15 15:15:27 -0700

    Fix sample script

--- api_sample.py
+++ api_sample.py
@@ -1,90 +1,74 @@
-from cryptography.x509 import NameOID
+from typing import cast
 
-from sslyze.concurrent_scanner import ConcurrentScanner, PluginRaisedExceptionScanResult
-from sslyze.plugins.certificate_info_plugin import CertificateInfoScanCommand
+from sslyze.connection_helpers.errors import ConnectionToServerFailed
+from sslyze.plugins.certificate_info.implementation import CertificateInfoScanResult
+from sslyze.plugins.compression_plugin import CompressionScanResult
+from sslyze.plugins.openssl_cipher_suites.implementation import CipherSuitesScanResult
+from sslyze.plugins.plugin_base import ScanCommandResult
+from sslyze.plugins.scan_commands import ScanCommandEnum
+from sslyze.scanner import ServerScanRequest, Scanner
+from sslyze.server_connectivity import ServerConnectivityTester
+from sslyze.server_setting import ServerNetworkLocationViaDirectConnection
 
-from sslyze.server_connectivity import ServerConnectivityTester, ServerConnectivityError
-from sslyze.ssl_settings import TlsWrappedProtocolEnum
-from sslyze.plugins.openssl_cipher_suites_plugin import Tlsv12ScanCommand, Tlsv10ScanCommand
-from sslyze.synchronous_scanner import SynchronousScanner
 
+def main() -> None:
+    # First validate that we can connect to the servers we want to scan
+    servers_to_scan = []
+    for hostname in ["cloudflare.com", "google.com"]:
+        server_location = ServerNetworkLocationViaDirectConnection.with_ip_address_lookup(hostname, 443)
+        try:
+            server_info = ServerConnectivityTester().perform(server_location)
+            servers_to_scan.append(server_info)
+        except ConnectionToServerFailed as e:
+            print(f"Error connecting to {server_location.hostname}:{server_location.port}: {e.error_message}")
+            return
 
-def demo_server_connectivity_tester():
-    try:
-        server_tester = ServerConnectivityTester(
-            hostname="smtp.gmail.com", port=587, tls_wrapped_protocol=TlsWrappedProtocolEnum.STARTTLS_SMTP
+    scanner = Scanner()
+
+    # Then queue some scan commands for each server
+    for server_info in servers_to_scan:
+        server_scan_req = ServerScanRequest(
+            server_info=server_info,
+            scan_commands={
+                ScanCommandEnum.TLS_1_0_CIPHER_SUITES,
+                ScanCommandEnum.TLS_1_1_CIPHER_SUITES,
+                ScanCommandEnum.TLS_1_2_CIPHER_SUITES,
+                ScanCommandEnum.CERTIFICATE_INFO,
+                ScanCommandEnum.TLS_COMPRESSION,
+            },
         )
-        print(f"\nTesting connectivity with {server_tester.hostname}:{server_tester.port}...")
-        server_info = server_tester.perform()
-    except ServerConnectivityError as e:
-        # Could not establish an SSL connection to the server
-        raise RuntimeError(f"Could not connect to {e.server_info.hostname}: {e.error_message}")
+        scanner.queue_scan(server_scan_req)
 
-    return server_info
+    # Then retrieve the result of the scan commands for each server
+    for server_scan_result in scanner.get_results():
+        print(f"\nResults for {server_scan_result.server_info.server_location.hostname}:")
 
+        # Scan commands that were run with no errors
+        for scan_command, result in server_scan_result.scan_commands_results.items():
+            if scan_command in [
+                ScanCommandEnum.TLS_1_0_CIPHER_SUITES,
+                ScanCommandEnum.TLS_1_1_CIPHER_SUITES,
+                ScanCommandEnum.TLS_1_2_CIPHER_SUITES,
+            ]:
+                typed_result = cast(CipherSuitesScanResult, result)
+                print(f"\nAccepted cipher suites for {scan_command.name}:")
+                for accepted_cipher_suite in typed_result.accepted_cipher_suites:
+                    print(f"* {accepted_cipher_suite.cipher_suite.name}")
 
-def demo_synchronous_scanner():
-    # Run one scan command to list the server's TLS 1.0 cipher suites
-    try:
-        server_tester = ServerConnectivityTester(
-            hostname="smtp.gmail.com", port=587, tls_wrapped_protocol=TlsWrappedProtocolEnum.STARTTLS_SMTP
-        )
-        print(f"\nTesting connectivity with {server_tester.hostname}:{server_tester.port}...")
-        server_info = server_tester.perform()
-    except ServerConnectivityError as e:
-        # Could not establish an SSL connection to the server
-        raise RuntimeError(f"Could not connect to {e.server_info.hostname}: {e.error_message}")
+            elif scan_command == ScanCommandEnum.CERTIFICATE_INFO:
+                typed_result = cast(CertificateInfoScanResult, result)
+                print("\nCertificate info:")
+                for cert_deployment in typed_result.certificate_deployments:
+                    print(f"Leaf certificate: \n{cert_deployment.verified_certificate_chain_as_pem[0]}")
 
-    command = Tlsv10ScanCommand()
+            elif scan_command == ScanCommandEnum.TLS_COMPRESSION:
+                typed_result = cast(CompressionScanResult, result)
+                print(f"\nCompression / CRIME: {typed_result.supports_compression}")
 
-    synchronous_scanner = SynchronousScanner()
-
-    scan_result = synchronous_scanner.run_scan_command(server_info, command)
-    for cipher in scan_result.accepted_cipher_list:
-        print(f"    {cipher.name}")
-
-
-def demo_concurrent_scanner():
-    # Setup the server to scan and ensure it is online/reachable
-    server_info = demo_server_connectivity_tester()
-
-    # Run multiple scan commands concurrently. It is much faster than the SynchronousScanner
-    concurrent_scanner = ConcurrentScanner()
-
-    # Queue some scan commands
-    print("\nQueuing some commands...")
-    concurrent_scanner.queue_scan_command(server_info, Tlsv12ScanCommand())
-    concurrent_scanner.queue_scan_command(server_info, CertificateInfoScanCommand())
-
-    # Process the results
-    print("\nProcessing results...")
-    for scan_result in concurrent_scanner.get_results():
-        # All scan results have the corresponding scan_command and server_info as an attribute
-        print(
-            f'\nReceived result for "{scan_result.scan_command.get_title()}" ' f"on {scan_result.server_info.hostname}"
-        )
-
-        # A scan command can fail (as a bug); it is returned as a PluginRaisedExceptionResult
-        if isinstance(scan_result, PluginRaisedExceptionScanResult):
-            raise RuntimeError(f"Scan command failed: {scan_result.scan_command.get_title()}")
-
-        # Each scan result has attributes with the information yo're looking for
-        # All these attributes are documented within each scan command's module
-        if isinstance(scan_result.scan_command, Tlsv12ScanCommand):
-            for cipher in scan_result.accepted_cipher_list:
-                print(f"    {cipher.name}")
-
-        elif isinstance(scan_result.scan_command, CertificateInfoScanCommand):
-            # Print the Common Names within the verified certificate chain
-            if not scan_result.verified_certificate_chain:
-                print("Error: certificate chain is not trusted!")
-            else:
-                print("Certificate chain common names:")
-                for cert in scan_result.verified_certificate_chain:
-                    cert_common_names = cert.subject.get_attributes_for_oid(NameOID.COMMON_NAME)
-                    print(f"   {cert_common_names[0].value}")
+        # Scan commands that were run with errors
+        for scan_command, error in server_scan_result.scan_commands_errors.items():
+            print(f"\nError when running {scan_command}:\n{error.exception_trace}" )
 
 
 if __name__ == "__main__":
-    demo_synchronous_scanner()
-    demo_concurrent_scanner()
+    main()

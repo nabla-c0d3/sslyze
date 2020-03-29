f3ec4ec196e21c4afd6e482a8f27a228580edb48
Alban Diquet <nabla.c0d3@gmail.com>
2020-03-29 16:16:38 -0700

    Fix dependency

--- setup.py
+++ setup.py
@@ -89,7 +89,7 @@ setup(
     entry_points={"console_scripts": ["sslyze = sslyze.__main__:main"]},
     # Dependencies
     install_requires=[
-        "nassl>=2.2.0,<2.3.0",
+        "nassl>=3.0.0,<3.2.0",
         "cryptography==2.5",
         "tls-parser>=1.2.0,<1.3.0",
         "typing_extensions ; python_version<'3.8'",  # To remove when we drop support for Python 3.7

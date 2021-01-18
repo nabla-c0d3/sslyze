1c090a3aee670b21ad98fe398eaf5728d2fc9fbf
Alban Diquet <nabla.c0d3@gmail.com>
2021-01-18 11:32:52 -0800

    Improve JSON output for certificates

--- sslyze/plugins/certificate_info/_json_output.py
+++ sslyze/plugins/certificate_info/_json_output.py
@@ -3,12 +3,13 @@
 from base64 import b64encode
 from dataclasses import dataclass, asdict
 from datetime import datetime
-from typing import Dict, Any, List, Optional, Union
+from typing import Dict, Any, List, Optional
 
 from cryptography import x509
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.backends.openssl.ocsp import _OCSPResponse
 from cryptography.hazmat.backends.openssl.x509 import _Certificate
+from cryptography.hazmat.primitives import hashes
 from cryptography.hazmat.primitives.asymmetric.rsa import RSAPublicKey
 from cryptography.hazmat.primitives.serialization import Encoding
 from cryptography.x509.ocsp import OCSPResponseStatus, load_der_ocsp_response
@@ -43,8 +44,14 @@ def _monkeypatch_to_fix_certificate_asdict() -> None:
 _monkeypatch_to_fix_certificate_asdict()
 
 
+@dataclass(frozen=True)
+class _ObjectIdentifierAsJson:
+    name: str
+    dotted_string: str
+
+
 def oid_to_json(obj: ObjectIdentifier) -> Dict[str, str]:
-    return {"name": obj._name, "dotted_string": obj.dotted_string}
+    return asdict(_ObjectIdentifierAsJson(name=obj._name, dotted_string=obj.dotted_string))
 
 
 # We use dataclasses here to ensure consistency in how we serialize X509 names
@@ -57,9 +64,8 @@ class _X509NameAttributeAsJson:
 
 @dataclass(frozen=True)
 class _X509NameAsJson:
-    rfc4514_string: Optional[str]  # None if parsing_error is set
-    attributes: Optional[List[_X509NameAttributeAsJson]]  # None if parsing_error is set
-    parsing_error: Optional[str]
+    rfc4514_string: str
+    attributes: List[_X509NameAttributeAsJson]
 
 
 def x509_name_to_json(name: x509.Name) -> Dict[str, Any]:
@@ -69,53 +75,114 @@ def x509_name_to_json(name: x509.Name) -> Dict[str, Any]:
             _X509NameAttributeAsJson(oid=attr.oid, value=attr.value, rfc4514_string=attr.rfc4514_string())
         )
 
-    x509name_as_json = _X509NameAsJson(rfc4514_string=name.rfc4514_string(), attributes=attributes, parsing_error=None)
+    x509name_as_json = _X509NameAsJson(rfc4514_string=name.rfc4514_string(), attributes=attributes)
     return asdict(x509name_as_json)
 
 
-def x509_certificate_to_json(certificate: x509.Certificate) -> Dict[str, Any]:
-    result: Dict[str, Union[None, str, Dict[str, Any]]] = {
-        # Add general info
-        "as_pem": certificate.public_bytes(Encoding.PEM).decode("ascii"),
-        "hpkp_pin": b64encode(get_public_key_sha256(certificate)).decode("utf-8"),  # RFC 7469
-        # Add some of the fields of the cert
-        "serialNumber": str(certificate.serial_number),
-        "notBefore": certificate.not_valid_before.isoformat(),
-        "notAfter": certificate.not_valid_after.isoformat(),
-        "subjectAlternativeName": {"DNS": extract_dns_subject_alternative_names(certificate)},
-    }
+@dataclass(frozen=True)
+class _PublicKeyAsJson:
+    algorithm: str
+    key_size: Optional[int]  # None for Ed25519PublicKey and Ed448PublicKey
 
+    # Only set if the algorithm is RSA
+    rsa_e: Optional[int]
+    rsa_n: Optional[int]
+
+    # Only set if the algorithm is Elliptic Curve
+    ec_curve_name: Optional[str]
+    ec_x: Optional[int]
+    ec_y: Optional[int]
+
+
+@dataclass(frozen=True)
+class _SubjAltNameAsJson:
+    dns: List[str]
+
+
+@dataclass(frozen=True)
+class _HashAlgorithmAsJson:
+    name: str
+    digest_size: int
+
+
+@dataclass(frozen=True)
+class _X509CertificateAsJson:
+    as_pem: str
+    hpkp_pin: str  # RFC 7469
+    fingerprint_sha1: str
+    fingerprint_sha256: str
+
+    serial_number: int
+    not_valid_before: datetime
+    not_valid_after: datetime
+    subject_alternative_name: _SubjAltNameAsJson
+
+    # The signature_hash_algorithm can be None if signature did not use separate hash (ED25519, ED448)
+    # https://cryptography.io/en/latest/x509/reference/#cryptography.x509.Certificate.signature_hash_algorithm
+    signature_hash_algorithm: Optional[_HashAlgorithmAsJson]
+    signature_algorithm_oid: ObjectIdentifier
+
+    # We may get garbage/invalid certificates that do not have a subject or an issuer, hence they can be None
+    # https://github.com/nabla-c0d3/sslyze/issues/403
+    subject: Optional[x509.name.Name]
+    issuer: Optional[x509.name.Name]
+
+    public_key: _PublicKeyAsJson
+
+
+def x509_certificate_to_json(certificate: x509.Certificate) -> Dict[str, Any]:
+    public_key = certificate.public_key()
+    public_key_json = _PublicKeyAsJson(
+        algorithm=public_key.__class__.__name__,
+        key_size=public_key.key_size if getattr(public_key, "key_size") else None,  # type: ignore
+        # EC-only fields
+        ec_curve_name=public_key.curve.name if isinstance(public_key, EllipticCurvePublicKey) else None,
+        ec_x=public_key.public_numbers().x if isinstance(public_key, EllipticCurvePublicKey) else None,
+        ec_y=public_key.public_numbers().y if isinstance(public_key, EllipticCurvePublicKey) else None,
+        # RSA-only fields
+        rsa_e=public_key.public_numbers().e if isinstance(public_key, RSAPublicKey) else None,
+        rsa_n=public_key.public_numbers().n if isinstance(public_key, RSAPublicKey) else None,
+    )
+
+    signature_hash_algorithm: Optional[_HashAlgorithmAsJson]
     if certificate.signature_hash_algorithm:
-        # The signature_hash_algorithm can be None if signature did not use separate hash (ED25519, ED448)
-        # https://cryptography.io/en/latest/x509/reference/#cryptography.x509.Certificate.signature_hash_algorithm
-        result["signatureAlgorithm"] = certificate.signature_hash_algorithm.name
+        signature_hash_algorithm = _HashAlgorithmAsJson(
+            name=certificate.signature_hash_algorithm.name,
+            digest_size=certificate.signature_hash_algorithm.digest_size,
+        )
     else:
-        result["signatureAlgorithm"] = None
+        signature_hash_algorithm = None
 
     # We may get garbage/invalid certificates so we need to handle ValueErrors.
     # See https://github.com/nabla-c0d3/sslyze/issues/403 for more information
-    for name_field in ["subject", "issuer"]:
-        try:
-            result[name_field] = getattr(certificate, name_field)
-        except ValueError as e:
-            x509name_as_json = _X509NameAsJson(rfc4514_string=None, attributes=None, parsing_error=e.args[0])
-            result[name_field] = asdict(x509name_as_json)
+    subject_field: Optional[x509.name.Name]
+    try:
+        subject_field = certificate.subject
+    except ValueError:
+        subject_field = None
 
-    # Add some info about the public key
-    public_key = certificate.public_key()
-    public_key_dict: Dict[str, Union[str, int]] = {"algorithm": public_key.__class__.__name__}
-    if isinstance(public_key, EllipticCurvePublicKey):
-        public_key_dict["size"] = public_key.curve.key_size
-        public_key_dict["curve"] = public_key.curve.name
-    elif isinstance(public_key, RSAPublicKey):
-        public_key_dict["size"] = public_key.key_size
-        public_key_dict["exponent"] = public_key.public_numbers().e
-    else:
-        # DSA Key? https://github.com/nabla-c0d3/sslyze/issues/402
-        pass
+    issuer_field: Optional[x509.name.Name]
+    try:
+        issuer_field = certificate.issuer
+    except ValueError:
+        issuer_field = None
 
-    result["publicKey"] = public_key_dict
-    return result
+    cert_as_json = _X509CertificateAsJson(
+        as_pem=certificate.public_bytes(Encoding.PEM).decode("ascii"),
+        hpkp_pin=b64encode(get_public_key_sha256(certificate)).decode("ascii"),
+        fingerprint_sha1=b64encode(certificate.fingerprint(hashes.SHA1())).decode("ascii"),
+        fingerprint_sha256=b64encode(certificate.fingerprint(hashes.SHA256())).decode("ascii"),
+        serial_number=certificate.serial_number,
+        not_valid_before=certificate.not_valid_before,
+        not_valid_after=certificate.not_valid_after,
+        subject_alternative_name=_SubjAltNameAsJson(dns=extract_dns_subject_alternative_names(certificate)),
+        signature_hash_algorithm=signature_hash_algorithm,
+        signature_algorithm_oid=certificate.signature_algorithm_oid,
+        subject=subject_field,
+        issuer=issuer_field,
+        public_key=public_key_json,
+    )
+    return asdict(cert_as_json)
 
 
 @dataclass(frozen=True)

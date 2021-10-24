ce1912ff6d3533ee73386e9b20b68f66dce0e215
Alban Diquet <ad@datatheorem.io>
2021-10-24 11:43:54 -0700

    Bump version number

--- sslyze/__version__.py
+++ sslyze/__version__.py
@@ -1,5 +1,5 @@
 __title__ = "SSLyze"
-__version__ = "5.0.0b0"
+__version__ = "5.0.0b1"
 __description__ = "Fast and powerful SSL/TLS scanning library."
 __author__ = "Alban Diquet"
 __author_email__ = "nabla.c0d3@gmail.com"

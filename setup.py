6503db221a98c85b02606a1f9a9aca8b5dd54e35
Alban Diquet <ad@datatheorem.io>
2022-01-01 15:16:13 +0100

    Update tls_parser

--- setup.py
+++ setup.py
@@ -100,7 +100,7 @@ setup(
     install_requires=[
         "nassl>=4.0.1,<5.0.0",
         "cryptography>=2.6,<37.0.0",
-        "tls-parser>=1.2.2,<1.3.0",
+        "tls-parser>=2.0.0,<3.0.0",
         "pydantic>=1.7,<1.9",
     ],
     # cx_freeze info for Windows builds with Python embedded

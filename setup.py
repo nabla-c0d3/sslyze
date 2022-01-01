dfe742ae7b563a08dfae00f317b0551d560a29cf
Alban Diquet <ad@datatheorem.io>
2022-01-01 11:38:07 +0100

    Add Python 3.10

--- setup.py
+++ setup.py
@@ -76,6 +76,7 @@ setup(
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
         "Topic :: System :: Networking",
         "Topic :: System :: Monitoring",
         "Topic :: System :: Networking :: Monitoring",

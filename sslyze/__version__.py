4ec80a38c7be787e5d05576f0f9b92920bc370b2
Alban Diquet <nabla.c0d3@gmail.com>
2023-01-18 19:59:24 +0100

    Bump version number

--- sslyze/__version__.py
+++ sslyze/__version__.py
@@ -1,5 +1,5 @@
 __title__ = "SSLyze"
-__version__ = "5.1.0"
+__version__ = "5.1.1"
 __description__ = "Fast and powerful SSL/TLS scanning library."
 __author__ = "Alban Diquet"
 __author_email__ = "nabla.c0d3@gmail.com"
